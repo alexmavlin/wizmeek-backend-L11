c6c6be0faf6628088c6bf7314ffcc081a52ce9d1
afalkon <info.dryguy@gmail.com>
2025-04-15 17:25:17 +0200

    Genre Imageges functionality added

--- api/app/Http/Controllers/Admin/Genres/GenresUpdateController.php
+++ api/app/Http/Controllers/Admin/Genres/GenresUpdateController.php
@@ -13,9 +13,22 @@ class GenresUpdateController extends Controller
         try {
             $genre->genre = $request->input('genre');
             $genre->color = $request->input('color');
+
+            if ($request->hasFile('image')) {
+                if ($genre->img_link && file_exists(public_path($genre->img_link))) {
+                    unlink(public_path($genre->img_link));
+                }
+
+                $image = $request->file('image');
+                $filename = uniqid('genre_') . '.' . $image->getClientOriginalExtension();
+                $image->move(public_path('genres'), $filename);
+
+                $genre->img_link = 'genres/' . $filename;
+            }
+
             $genre->save();
         } catch (\Exception $error) {
-            return redirect()->back()->with('error', 'An error has occured during an attempt to update a genre. Error: ' . $error->getMessage());
+            return redirect()->back()->with('error', 'An error has occurred during an attempt to update a genre. Error: ' . $error->getMessage());
         }
 
         return redirect()->route('admin_genres_view', $genre)->with('success', 'Genre updated successfully.');
