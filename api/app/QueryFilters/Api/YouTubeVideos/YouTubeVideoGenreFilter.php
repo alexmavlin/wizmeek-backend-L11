e1dae0324c201c7533b55b46ecaf4d0a1f744a3a
bedddev <bedddev18@gmail.com>
2025-09-12 09:07:10 +0700

    Fix query for genre changes

--- api/app/QueryFilters/Api/YouTubeVideos/YouTubeVideoGenreFilter.php
+++ api/app/QueryFilters/Api/YouTubeVideos/YouTubeVideoGenreFilter.php
@@ -10,10 +10,20 @@ class YouTubeVideoGenreFilter
     {
         $genre = request()->header('X-Genre');
 
-        if ($genre && is_numeric($genre)) {
-            $query->whereHas('genre', function ($q) use ($genre) {
-                $q->where('genre', 'like', '%' . $genre . '%');
-            });
+        \Log::info($genre);
+
+        if ($genre && $genre !== '' && $genre !== 'All') {
+            // If genre is numeric, treat it as an ID
+            if (is_numeric($genre)) {
+                $query->whereHas('genre', function ($q) use ($genre) {
+                    $q->where('id', $genre);
+                });
+            } else {
+                // If genre is a string, treat it as a label
+                $query->whereHas('genre', function ($q) use ($genre) {
+                    $q->where('genre', 'like', '%' . $genre . '%');
+                });
+            }
         }
 
         return $next($query);
